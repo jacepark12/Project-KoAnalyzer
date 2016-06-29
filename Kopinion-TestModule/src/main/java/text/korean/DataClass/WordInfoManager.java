cd40e5daf58ba2d2e990c06551b8fd60e10ece35
박재성 <jspark141515@gmail.com>
2016-06-29 20:46:39 +0900

    Created WordInfoManager class for saving WordInfo ArrayList as one static object in application

---
+++ Kopinion-TestModule/src/main/java/text/korean/DataClass/WordInfoManager.java
@@ -0,0 +1,16 @@
+package text.korean.DataClass;
+
+import java.util.ArrayList;
+
+/**
+ * Created by parkjaesung on 2016. 6. 29..
+ */
+public class WordInfoManager {
+
+    public static ArrayList<WordInfo> wordInfoArrayList = new ArrayList<>();
+    //private constructor
+    private WordInfoManager(){
+
+    }
+
+}

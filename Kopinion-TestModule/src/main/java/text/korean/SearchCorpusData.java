bb2cd6fa1e65d2a67071e6719ad411aa9bb1789c
박재성 <jspark141515@gmail.com>
2016-06-29 22:39:42 +0900

    Changed the algorithms of searching word data from sentiment corpus

--- Kopinion-TestModule/src/main/java/text/korean/SearchCorpusData.java
+++ Kopinion-TestModule/src/main/java/text/korean/SearchCorpusData.java
@@ -14,16 +14,17 @@ public class SearchCorpusData{
     //set filePath in property file
     private static String filePath = System.getProperty("user.dir") + "/Kopinion-TestModule/src/main/resources/TextData/corpus.csv";
 
-    private CSVReader reader;
-    private static SearchCorpusData instance;
+    //private CSVReader reader;
+    private static SearchCorpusData instance =  new SearchCorpusData();
 
     //constructor
     private SearchCorpusData(){
+/*
         try {
             this.reader = new CSVReader(new FileReader(filePath));
         } catch (FileNotFoundException e) {
             e.printStackTrace();
-        }
+        }*/
     }
 
     public static SearchCorpusData getInstance(){
@@ -34,35 +35,61 @@ public class SearchCorpusData{
     //true : if word has positive sentiment
     //false : if word has negative sentiment
     //리팩토링 필요 : 긍정이 아니면 무조건 false이므로, 중립적인단어임에도 불구하고 부정으로 판별될 수 있음
-    public boolean isWordPositive(CharSequence word)throws IOException, NoWordDataException{
+    // TODO What if there is no word data in corpus?
+    public boolean isWordPositive(CharSequence word)throws IOException{
+        CSVReader reader = new CSVReader(new FileReader(filePath));
+
+        System.out.println("isWordPositive!");
         boolean result = false;
         String[] nextLine;
 
         boolean isWordDataExists = false;
+
         while ((nextLine = reader.readNext()) != null) {
             // nextLine[] is an array of values from the line
             //System.out.println("단어 : " + nextLine[3]);
+            if(nextLine[2].equals("Seed")){
+                if(nextLine[3].matches(".*"+word +"*.") || nextLine[13].contains(word)){
+                    System.out.println("긍정 혹은 부정 : " + nextLine[6]);
 
-            if(nextLine[3].contains(word)){
-                System.out.println("긍정 혹은 부정 : " + nextLine[6]);
+                    isWordDataExists = true;
 
-                isWordDataExists = true;
+                    switch (nextLine[6]){
+                        case "POS" : result = true;
+                            break;
+                        case "NEG" : result = false;
+                            break;
+                        default: result = false;
 
-                switch (nextLine[6]){
-                    case "POS" : result = true;
-                        break;
-                    case "NEG" : result = false;
-                        break;
-                    default: result = false;
-
+                    }
+                    break;
                 }
-                break;
+        }else if(nextLine[2].equals("SubjTag")){
+                if(nextLine[3].matches(".*"+word +"*.") || nextLine[13].contains(word)){
+                    System.out.println("긍정 혹은 부정 : " + nextLine[7]);
+
+                    isWordDataExists = true;
+
+                    switch (nextLine[7]){
+                        case "POS" : result = true;
+                            break;
+                        case "NEG" : result = false;
+                            break;
+                        default: result = false;
+
+                    }
+                    break;
+                }
             }
+
         }
 
         if(!isWordDataExists){
-            throw  new NoWordDataException("No word :" + word.toString() + " in corpus data");
+            System.out.println(word.toString() + " 데이터가 검색되지 않음!");
+        }else{
+            System.out.println("데이터 있음!!");
         }
+
         return result;
 
     }

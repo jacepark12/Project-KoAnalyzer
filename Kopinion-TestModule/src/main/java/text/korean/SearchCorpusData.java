31797cdd79fcc75af3aa80070bdb6ccc2fb07233
박재성 <jspark141515@gmail.com>
2016-06-29 21:02:35 +0900

    Changed SearchCorpusData class to singleton pattern
    Completed isWordPositive method

--- Kopinion-TestModule/src/main/java/text/korean/SearchCorpusData.java
+++ Kopinion-TestModule/src/main/java/text/korean/SearchCorpusData.java
@@ -2,6 +2,7 @@ package text.korean;
 
 import com.opencsv.CSVReader;
 
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 
@@ -13,18 +14,56 @@ public class SearchCorpusData{
     //set filePath in property file
     private static String filePath = System.getProperty("user.dir") + "/Kopinion-TestModule/src/main/resources/TextData/corpus.csv";
 
-    public static void isNegOrPos(CharSequence word) throws IOException{
-        System.out.println(filePath);
+    private CSVReader reader;
+    private static SearchCorpusData instance;
 
-        CSVReader reader = new CSVReader(new FileReader(filePath));
+    //constructor
+    private SearchCorpusData(){
+        try {
+            this.reader = new CSVReader(new FileReader(filePath));
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static SearchCorpusData getInstance(){
+        return instance;
+    }
+
+
+    //true : if word has positive sentiment
+    //false : if word has negative sentiment
+    //리팩토링 필요 : 긍정이 아니면 무조건 false이므로, 중립적인단어임에도 불구하고 부정으로 판별될 수 있음
+    public boolean isWordPositive(CharSequence word)throws IOException, NoWordDataException{
+        boolean result = false;
         String[] nextLine;
+
+        boolean isWordDataExists = false;
         while ((nextLine = reader.readNext()) != null) {
             // nextLine[] is an array of values from the line
             //System.out.println("단어 : " + nextLine[3]);
 
             if(nextLine[3].contains(word)){
                 System.out.println("긍정 혹은 부정 : " + nextLine[6]);
+
+                isWordDataExists = true;
+
+                switch (nextLine[6]){
+                    case "POS" : result = true;
+                        break;
+                    case "NEG" : result = false;
+                        break;
+                    default: result = false;
+
+                }
+                break;
             }
         }
+
+        if(!isWordDataExists){
+            throw  new NoWordDataException("No word :" + word.toString() + " in corpus data");
+        }
+        return result;
+
     }
 }

9faeafc922c7cf304d35ac9eb2ae7f9aa7459275
박재성 <jspark141515@gmail.com>
2016-06-30 12:35:26 +0900

    SearchCorpusData class now implements SentimentTypeInferace for using SentimentType enumeration
    Extracted getSentimentFromCol method
    
    isWordPositive method return type has  changed to SentimentType

--- Kopinion-TestModule/src/main/java/text/korean/SearchCorpusData.java
+++ Kopinion-TestModule/src/main/java/text/korean/SearchCorpusData.java
@@ -1,6 +1,7 @@
 package text.korean;
 
 import com.opencsv.CSVReader;
+import text.korean.DataClass.SentimentTypeInterface;
 
 import java.io.FileNotFoundException;
 import java.io.FileReader;
@@ -9,13 +10,14 @@ import java.io.IOException;
 /**
  * Created by parkjaesung on 2016. 6. 29..
  */
-public class SearchCorpusData{
+public class SearchCorpusData implements SentimentTypeInterface{
 
     //set filePath in property file
     private static String filePath = System.getProperty("user.dir") + "/Kopinion-TestModule/src/main/resources/TextData/corpus.csv";
 
     //private CSVReader reader;
     private static SearchCorpusData instance =  new SearchCorpusData();
+
 
     //constructor
     private SearchCorpusData(){
@@ -31,63 +33,82 @@ public class SearchCorpusData{
         return instance;
     }
 
+    // if tag type == Seed => get sentiment from index 6
+    // if tag type == SubjTag => get sentiment from index 7
+    public SentimentType getSentimentFromCol(String[] nextLine, CharSequence word, int index) {
+
+        SentimentType result = SentimentType.NODATA;
+
+        if (nextLine[3].matches(".*" + word + "*.") || nextLine[13].contains(word)) {
+
+            //  isWordDataExists = true;
+            switch (nextLine[index]) {
+                case "POS":
+                    result = SentimentType.POS;
+                    break;
+                case "NEG":
+                    result = SentimentType.NEG;
+                    break;
+                case "NEUT":
+                    result = SentimentType.NEUT;
+                    break;
+                default:
+                    result = SentimentType.NODATA;
+
+            }
+        }
+
+        return result;
+    }
 
     //true : if word has positive sentiment
     //false : if word has negative sentiment
     //리팩토링 필요 : 긍정이 아니면 무조건 false이므로, 중립적인단어임에도 불구하고 부정으로 판별될 수 있음
     // TODO What if there is no word data in corpus?
-    public boolean isWordPositive(CharSequence word)throws IOException{
+    public SentimentType isWordPositive(CharSequence word)throws IOException{
+        SentimentType result = SentimentType.NODATA;
+
+        int posCount = 0;
+        int negCount = 0;
+
         CSVReader reader = new CSVReader(new FileReader(filePath));
 
-        System.out.println("isWordPositive!");
-        boolean result = false;
         String[] nextLine;
 
-        boolean isWordDataExists = false;
-
+        SentimentType sentimentType;
         while ((nextLine = reader.readNext()) != null) {
             // nextLine[] is an array of values from the line
-            //System.out.println("단어 : " + nextLine[3]);
-            if(nextLine[2].equals("Seed")){
-                if(nextLine[3].matches(".*"+word +"*.") || nextLine[13].contains(word)){
-                    System.out.println("긍정 혹은 부정 : " + nextLine[6]);
 
-                    isWordDataExists = true;
+            switch (nextLine[2]){
+                case "Seed" :
+                    sentimentType = getSentimentFromCol(nextLine, word, 6);
 
-                    switch (nextLine[6]){
-                        case "POS" : result = true;
-                            break;
-                        case "NEG" : result = false;
-                            break;
-                        default: result = false;
-
+                    if(sentimentType.equals(SentimentType.POS)){
+                        posCount++;
+                    }else if(sentimentType.equals(SentimentType.NEG)){
+                        negCount++;
                     }
                     break;
-                }
-        }else if(nextLine[2].equals("SubjTag")){
-                if(nextLine[3].matches(".*"+word +"*.") || nextLine[13].contains(word)){
-                    System.out.println("긍정 혹은 부정 : " + nextLine[7]);
 
-                    isWordDataExists = true;
+                case "SubjTag" :
+                    sentimentType = getSentimentFromCol(nextLine, word, 7);
 
-                    switch (nextLine[7]){
-                        case "POS" : result = true;
-                            break;
-                        case "NEG" : result = false;
-                            break;
-                        default: result = false;
-
+                    if(sentimentType.equals(SentimentType.POS)){
+                        posCount++;
+                    }else if(sentimentType.equals(SentimentType.NEG)){
+                        negCount++;
                     }
+
                     break;
-                }
             }
-
         }
 
-        if(!isWordDataExists){
-            System.out.println(word.toString() + " 데이터가 검색되지 않음!");
+        if(posCount > negCount){
+            result = SentimentType.POS;
+        }else if(posCount < negCount){
+            result = SentimentType.NEG;
         }else{
-            System.out.println("데이터 있음!!");
+            result = SentimentType.NODATA;
         }
 
         return result;

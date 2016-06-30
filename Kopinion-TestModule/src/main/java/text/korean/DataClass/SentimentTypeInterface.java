f6f1f4eaa927be37d8ec102c8e834eb02229ede3
박재성 <jspark141515@gmail.com>
2016-06-30 12:30:59 +0900

    Created interface SentimentTypeInterface
    Has Member variable(public static final) SentimentType enumeration

---
+++ Kopinion-TestModule/src/main/java/text/korean/DataClass/SentimentTypeInterface.java
@@ -0,0 +1,10 @@
+package text.korean.DataClass;
+
+/**
+ * Created by parkjaesung on 2016. 6. 30..
+ */
+public interface SentimentTypeInterface {
+
+    enum SentimentType {POS, NEG, NEUT, NODATA}
+
+}

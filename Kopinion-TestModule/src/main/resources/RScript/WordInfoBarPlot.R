cd446e67152cafc467b61ab226d08df7f523e3e3
박재성 <jspark141515@gmail.com>
2016-07-05 11:30:34 +0900

    Updated col colors, xlab, ylab in BarPlot

--- Kopinion-TestModule/src/main/resources/RScript/WordInfoBarPlot.R
+++ Kopinion-TestModule/src/main/resources/RScript/WordInfoBarPlot.R
@@ -4,4 +4,7 @@ names(sentiment_csv) = c("0", "10", "20", "30", "40", "50", "60", "70", "80", "9
 
 sentiment_matrix = as.matrix(sentiment_csv)
 
-barplot(sentiment_matrix)
+barplot(sentiment_matrix,
+        col = c("blue","red","gray"),
+        xlab = "Position in Text",
+        ylab = "Number of Sentiment Words")

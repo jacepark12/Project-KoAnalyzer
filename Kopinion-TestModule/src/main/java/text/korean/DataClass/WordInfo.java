56df6eae777f6b8fe551a0873878f9c88b8a99b7
박재성 <jspark141515@gmail.com>
2016-07-07 15:55:25 +0900

    Deleted WordDistribution Manager
    Changed WordDenstiy class to WordInfo class

--- Kopinion-TestModule/src/main/java/text/korean/DataClass/WordInfo.java
+++ Kopinion-TestModule/src/main/java/text/korean/DataClass/WordInfo.java
@@ -1,13 +1,53 @@
 package text.korean.DataClass;
 
+import java.util.ArrayList;
+
 /**
- * Created by parkjaesung on 2016. 6. 29..
+ * Created by parkjaesung on 2016. 7. 2..
  */
 public class WordInfo implements SentimentTypeInterface{
 
-    private StringBuffer word;
+    private String word;
+    //Variable positionAvg's value is only est by private method calculateDistribution
+    private double positionAvg;
+    private ArrayList<Double> positionArrayList = new ArrayList();
+
+    //Variable density's value is only est by private method calculateDistribution
+    private double density;
+    private int posAtTextPerCentage; //save as percentage by multiples of 10
     private SentimentType sentimentType;
-    private int positionAtText; //save as percentage by multiples of 10
+
+    public void addDataToPositionArrayList(double input){
+        positionArrayList.add(input);
+    }
+
+    public int getPosAtTextPerCentage() {
+        return posAtTextPerCentage;
+    }
+
+    public void setPosAtTextPerCentage(int posAtTextPerCentage) {
+        this.posAtTextPerCentage = posAtTextPerCentage;
+    }
+
+    public String getWord() {
+        return word;
+    }
+
+    public void setWord(String word) {
+        this.word = word;
+    }
+
+    public double getPositionAvg() {
+        return positionAvg;
+    }
+
+    public ArrayList getPositionArrayList() {
+        return positionArrayList;
+    }
+
+    public double getDensity() {
+        return density;
+    }
 
     public SentimentType getSentimentType() {
         return sentimentType;
@@ -17,19 +57,21 @@ public class WordInfo implements SentimentTypeInterface{
         this.sentimentType = sentimentType;
     }
 
-    public StringBuffer getWord() {
-        return word;
-    }
+    public void calculateDensity(){
 
-    public void setWord(StringBuffer word) {
-        this.word = word;
-    }
+        //Get Average
+        double sum = 0;
+        for (Double position : this.positionArrayList) {
+            sum+=position;
+        }
+        this.positionAvg = sum / this.positionArrayList.size()+1;
 
-    public int getPositionAtText() {
-        return positionAtText;
-    }
+        //Calculate Distribution
+        double distributionSum = 0;
+        for (Double position : this.positionArrayList) {
+            distributionSum += Math.pow(this.positionAvg - position,2);
+        }
 
-    public void setPositionAtText(int positionAtText) {
-        this.positionAtText = positionAtText;
+        this.density = distributionSum / this.positionArrayList.size()+1;
     }
 }

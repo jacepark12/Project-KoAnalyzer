f14952f028af35bc56dc2e44cbc950f54f5d82d2
박재성 <jspark141515@gmail.com>
2016-07-23 11:36:33 +0900

    Sample of redirecting jsp page

---
+++ KoAnalyzer-Server/src/main/java/com/analyze/ko/framework/server/servlet/TestJSP.java
@@ -0,0 +1,21 @@
+package com.analyze.ko.framework.server.servlet;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Created by parkjaesung on 2016. 7. 22..
+ */
+public class TestJSP extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        RequestDispatcher rd;
+        rd = getServletContext().getRequestDispatcher("/test.jsp");
+
+        rd.forward(req,resp);
+    }
+}
---
+++ KoAnalyzer-Server/src/main/webapp1/test.jsp
@@ -0,0 +1,11 @@
+<%@ page contentType="text/html; charset=euc-kr" %>
+<html>
+
+<head>
+
+</head>
+
+<body>
+test
+</body>
+</html>

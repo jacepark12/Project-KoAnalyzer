849eabd012b9faa1d5ef5eaf08583b745fa013db
박재성 <jspark141515@gmail.com>
2016-09-06 20:25:37 +0900

    Added normalizeText method in NormalizationManger class

--- KoAnalyzer-APIServer/src/main/java/com/KoAnalyzer/APIServer/normalization/NormalizationManager.java
+++ KoAnalyzer-APIServer/src/main/java/com/KoAnalyzer/APIServer/normalization/NormalizationManager.java
@@ -1,11 +1,16 @@
 package com.KoAnalyzer.APIServer.normalization;
 
+import com.twitter.penguin.korean.TwitterKoreanProcessorJava;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Service;
+
 /**
  * Created by parkjaesung on 2016. 9. 6..
  * Singleton Class
  * NormalizationManager works with NormalizationText class
  * Get input as NormalizationText and return Normalization class
  */
+@Service
 public class NormalizationManager {
 
     private static NormalizationManager instance = new NormalizationManager();
@@ -16,6 +21,14 @@ public class NormalizationManager {
 
     public static NormalizationManager getInstance(){
         return instance;
+    }
+
+    public NormalizationText normalizeText(NormalizationText normalizationText){
+
+        CharSequence normalized = TwitterKoreanProcessorJava.normalize(normalizationText.getOriginalText());
+        normalizationText.setNormalizatedText(normalized.toString());
+
+        return normalizationText;
     }
 
 }

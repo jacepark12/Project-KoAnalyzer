a2f89aed43153b0a9fe3495d74e002c0c3cd7c53
박재성 <jspark141515@gmail.com>
2016-09-09 22:30:00 +0900

    Created test class for testing StemmingManager class

---
+++ KoAnalyzer-APIServer/src/test/java/com/KoAnalyzer/APIServer/stemming/StemmingManagerTest.java
@@ -0,0 +1,26 @@
+package com.KoAnalyzer.APIServer.stemming;
+
+import com.KoAnalyzer.APIServer.Phrase;
+import org.junit.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by parkjaesung on 2016. 9. 9..
+ */
+public class StemmingManagerTest {
+
+    public static void main(String[] args){
+        StemmingManager stemmingManager = StemmingManager.getInstance();
+
+        StemmingText stemmingText = new StemmingText("이것이 예시입니닼ㅋ");
+        ArrayList<Phrase> stemmed = (ArrayList<Phrase>) stemmingManager.stemText(stemmingText).getPhrases();
+    }
+
+    @Test
+    public void testStemText() throws Exception {
+
+    }
+}

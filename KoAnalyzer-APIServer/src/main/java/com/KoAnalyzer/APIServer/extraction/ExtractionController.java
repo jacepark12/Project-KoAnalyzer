289c8198539c4c6c525d389f5b9f73c6e70fc107
박재성 <jspark141515@gmail.com>
2016-09-09 20:17:51 +0900

    Added getExtractionText method in ExtractionController

--- KoAnalyzer-APIServer/src/main/java/com/KoAnalyzer/APIServer/extraction/ExtractionController.java
+++ KoAnalyzer-APIServer/src/main/java/com/KoAnalyzer/APIServer/extraction/ExtractionController.java
@@ -1,6 +1,9 @@
 package com.KoAnalyzer.APIServer.extraction;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -9,4 +12,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/extraction")
 @RestController
 public class ExtractionController {
+
+    @Autowired
+    ExtractionManager extractionManager;
+
+    @RequestMapping(value = "/{text}", method = RequestMethod.GET)
+    public ExtractionText getExtractionText(@PathVariable("text")String originalText){
+        ExtractionText extractionText = new ExtractionText(originalText);
+
+        return extractionManager.extractText(extractionText);
+    }
 }

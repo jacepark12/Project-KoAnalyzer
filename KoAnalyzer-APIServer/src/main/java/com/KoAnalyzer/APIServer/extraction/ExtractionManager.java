e575a233d8108a56be77ab04eda50fdc745791d3
박재성 <jspark141515@gmail.com>
2016-09-09 20:49:59 +0900

    Added convertKoreanPhrase method in ExtractionManager class
    convertKoreanPhrase method is used at extracText method

--- KoAnalyzer-APIServer/src/main/java/com/KoAnalyzer/APIServer/extraction/ExtractionManager.java
+++ KoAnalyzer-APIServer/src/main/java/com/KoAnalyzer/APIServer/extraction/ExtractionManager.java
@@ -1,11 +1,13 @@
 package com.KoAnalyzer.APIServer.extraction;
 
+import com.KoAnalyzer.APIServer.Phrase;
 import com.twitter.penguin.korean.TwitterKoreanProcessorJava;
 import com.twitter.penguin.korean.phrase_extractor.KoreanPhraseExtractor;
 import com.twitter.penguin.korean.tokenizer.KoreanTokenizer;
 import org.springframework.stereotype.Service;
 import scala.collection.Seq;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -30,9 +32,26 @@ public class ExtractionManager {
         Seq<KoreanTokenizer.KoreanToken> tokens = TwitterKoreanProcessorJava.tokenize(extractionText.getOriginalText());
         List<KoreanPhraseExtractor.KoreanPhrase> phrases = TwitterKoreanProcessorJava.extractPhrases(tokens, true, true);
 
-        extractionText.setPhrases(phrases);
+        extractionText.setPhrases(convertKoreanPhrase(phrases));
 
         return extractionText;
+    }
+
+    public List<Phrase> convertKoreanPhrase(List<KoreanPhraseExtractor.KoreanPhrase> phrases){
+        ArrayList<Phrase> converted = new ArrayList<Phrase>();
+
+        for (KoreanPhraseExtractor.KoreanPhrase phrase: phrases) {
+            Phrase convertedPhrase = new Phrase();
+
+            convertedPhrase.setText(phrase.text());
+            convertedPhrase.setPos(phrase.pos().toString());
+            convertedPhrase.setOffset(phrase.offset());
+            convertedPhrase.setLength(phrase.length());
+
+            converted.add(convertedPhrase);
+        }
+
+        return converted;
     }
 }
 

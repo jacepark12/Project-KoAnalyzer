42763208f123b14a2a89be7c01dbcf838a0bc643
박재성 <jspark141515@gmail.com>
2016-09-09 20:19:02 +0900

    Added Console write code in ExtractionManagerTest class for searching values of KoreanPhrase

--- KoAnalyzer-APIServer/src/test/java/com/KoAnalyzer/APIServer/extraction/ExtractionManagerTest.java
+++ KoAnalyzer-APIServer/src/test/java/com/KoAnalyzer/APIServer/extraction/ExtractionManagerTest.java
@@ -16,8 +16,13 @@ public class ExtractionManagerTest {
         ExtractionText extractionText = new ExtractionText(inputText);
 
         ExtractionManager extractionManager = ExtractionManager.getInstance();
-        System.out.println("result : "  +extractionManager.extractText(extractionText).getPhrases() ) ;
 
+        System.out.println("result : "  +extractionManager.extractText(extractionText).getPhrases() ) ;
+        System.out.println("text : "  +extractionManager.extractText(extractionText).getPhrases().get(0).text() ) ;
+        System.out.println("productPrefix : "  +extractionManager.extractText(extractionText).getPhrases().get(0).productPrefix() ) ;
+        System.out.println("length : "  +extractionManager.extractText(extractionText).getPhrases().get(0).length() ) ;
+        System.out.println("pos : "  +extractionManager.extractText(extractionText).getPhrases().get(0).pos() ) ;
+        System.out.println("offset : "  +extractionManager.extractText(extractionText).getPhrases().get(0).offset() ) ;
     }
 
     @Test

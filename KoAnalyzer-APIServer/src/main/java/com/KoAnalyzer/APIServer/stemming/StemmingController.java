54d4c90b0c3309409a7a44efee8ab8ccab33d785
박재성 <jspark141515@gmail.com>
2016-09-09 22:30:22 +0900

    Completed StemmingController

--- KoAnalyzer-APIServer/src/main/java/com/KoAnalyzer/APIServer/stemming/StemmingController.java
+++ KoAnalyzer-APIServer/src/main/java/com/KoAnalyzer/APIServer/stemming/StemmingController.java
@@ -1,6 +1,10 @@
 package com.KoAnalyzer.APIServer.stemming;
 
+import com.KoAnalyzer.APIServer.normalization.NormalizationText;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -9,4 +13,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/stemming")
 @RestController
 public class StemmingController {
+
+    @Autowired
+    StemmingManager stemmingManager;
+
+    @RequestMapping(value = "/{text}", method = RequestMethod.GET)
+    public StemmingText getStemmedText(@PathVariable("text")String originalText){
+        StemmingText stemmingText = new StemmingText(originalText);
+
+        return stemmingManager.stemText(stemmingText);
+    }
 }

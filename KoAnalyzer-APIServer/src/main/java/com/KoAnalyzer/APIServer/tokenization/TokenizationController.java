09cc1ed639911f9c6ffbcdcc4e9654e58619c720
박재성 <jspark141515@gmail.com>
2016-09-12 14:14:59 +0900

    Created TokenizationText method for /tokenization/{text} GET

--- KoAnalyzer-APIServer/src/main/java/com/KoAnalyzer/APIServer/tokenization/TokenizationController.java
+++ KoAnalyzer-APIServer/src/main/java/com/KoAnalyzer/APIServer/tokenization/TokenizationController.java
@@ -1,6 +1,9 @@
 package com.KoAnalyzer.APIServer.tokenization;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -10,4 +13,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/tokenization")
 @RestController
 public class TokenizationController {
+
+    @Autowired
+    TokenizationManger tokenizationManger;
+
+    @RequestMapping(value = "/{text}", method = RequestMethod.GET)
+    public TokenizationText getTokenizedText(@PathVariable("text")String originalText){
+        TokenizationText tokenizationText = new TokenizationText(originalText);
+
+        return tokenizationManger.tokenizeText(tokenizationText);
+    }
 }

7bc20c7b2e646f58e909471474063dd5c201f248
박재성 <jspark141515@gmail.com>
2016-09-17 16:38:10 +0900

    Added id / task /count member variable in Phrase class
    Changed Phrase class as Entity bean

--- KoAnalyzer-APIServer/src/main/java/com/KoAnalyzer/APIServer/Phrase.java
+++ KoAnalyzer-APIServer/src/main/java/com/KoAnalyzer/APIServer/Phrase.java
@@ -1,15 +1,26 @@
 package com.KoAnalyzer.APIServer;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
 /**
  * Created by parkjaesung on 2016. 9. 9..
  * Phrase class is used for Stemming, Extraction
  */
+@Entity
 public class Phrase {
+
+    @Id
+    @GeneratedValue
+    private long id;
 
     private String text;
     private String pos;
+    private String task;
     private int length;
     private int offset;
+    private int count; //자연어 처리 결과로 사용된 횟수
 
     //getter & setter
 
@@ -43,5 +54,29 @@ public class Phrase {
 
     public void setOffset(int offset) {
         this.offset = offset;
+    }
+
+    public String getTask() {
+        return task;
+    }
+
+    public void setTask(String task) {
+        this.task = task;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    public void setCount(int count) {
+        this.count = count;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
     }
 }

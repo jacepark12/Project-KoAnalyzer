fd69f2e7bb060efda2b943436993fad50dd17042
박재성 <jspark141515@gmail.com>
2016-09-17 16:41:35 +0900

    Add @EnableJpaRepositories & @EntityScan annotations for APIServerLauncher class

--- KoAnalyzer-APIServer/src/main/java/com/KoAnalyzer/APIServer/APIServerLauncher.java
+++ KoAnalyzer-APIServer/src/main/java/com/KoAnalyzer/APIServer/APIServerLauncher.java
@@ -1,17 +1,21 @@
 package com.KoAnalyzer.APIServer;
 
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 /**
  * Created by parkjaesung on 2016. 8. 31..
  */
 
 @Configuration
+@EnableJpaRepositories
 @EnableAutoConfiguration
 @ComponentScan
+@EntityScan
 @PropertySource("config.properties")
 public class APIServerLauncher {
 
